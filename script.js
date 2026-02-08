2744027713d4f4c6fbd9854d59ff87e0cd0a014b
neooza1 <neokale10@gmail.com>
2026-02-08 15:36:46 +0200

    Update on render function

--- script.js
+++ script.js
@@ -2,27 +2,51 @@ let applications =[];
 
 const form = document.getElementById("applicationForm");
 
-form.addEventListener("Submit" , function(e){
-    (e).preventDefault();
+form.addEventListener("submit" , function(e){
+    
+    e.preventDefault();
+   
 
     const application={
         id:Date.now(),
-        studentName : document.getElementById("studentName"),
-        studentNumber : document.getElementById("studentNumber"),
-        faculty : document.getElementById("faculty"),
-        course : document.getElementById("course"),
+        studentName : document.getElementById("studentName").value,
+        studentNumber : document.getElementById("studentNumber").value,
+        faculty : document.getElementById("faculty").value,
+        course : document.getElementById("course").value,
         status : "pending"
 
-    }
+        
 
-    function addApplication(){
-        applications.push(application);
+    }
+    applications.push(application);
+    renderApplications();
+    console.log(applications);
+     form.reset();
+  
+    function renderApplications(){
+        
+        const list = document.getElementById("applicationList");
+        list.innerHTML="";
+
+    for( let i = 0; i <applications.length ; i++){
+        const app = applications[i];
+
+        const li = document.createElement("li");
+        li.textContent = ' ${app.studentName} | ${app.studentNumber} | ${app.course} | ${app.status}';
+        list.appendChild(li);
+
+
+    }
+   
+   
+        
+
+        
+       
+   
+
     }
     
-    function viewApplication(){
-        
-    }
-
 
 
 })
